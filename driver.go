6008514d27c96f2a691c7fd9c08892d1f4df7c23
Bean <pengshaobin@deepexi.com>
2023-11-20 15:08:27 +0800

    fix(driver): text/template

--- driver.go
+++ driver.go
@@ -9,19 +9,24 @@ import (
 )
 
 // FastTemplate is driver by fasttemplate
-func FastTemplate(content string, data map[string]interface{}, config *Config) io.Reader{
-    t := fasttemplate.New(content, config.FileStartTag, config.FileEndTag)
-    return strings.NewReader(t.ExecuteString(data))
+func FastTemplate(content string, data map[string]interface{}, config *Config) io.Reader {
+	t := fasttemplate.New(content, config.FileStartTag, config.FileEndTag)
+	return strings.NewReader(t.ExecuteString(data))
 }
 
 // TextTemplate is driver by text/template
 func TextTemplate(content string, data map[string]interface{}, config *Config) io.Reader {
-    tmpl, err := template.New(AppName).Delims(config.FileStartTag, config.FileEndTag).Parse(content)
-    if err != nil{
-        panic(err)
-    }
-    reader, writer := io.Pipe()  
-    tmpl.Execute(writer, data)
-    return reader
-
+	tmpl, err := template.New(AppName).Parse(content)
+	if err != nil {
+		panic(err)
+	}
+	reader, writer := io.Pipe()
+	go func() {
+		defer writer.Close()
+		err = tmpl.Execute(writer, data)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	return reader
 }
